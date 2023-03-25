18c2ab27a9d98f624e48ae50f2965bd80fb32a5e
THEEPORI <theepori92@gmail.com>
2023-03-25 20:38:29 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -14,7 +14,7 @@ class Development(Config):
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "5337555859:AAHGbL2Khcy0l8tDqIWOg05Gp9MRcPE3L64"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = "-1001121551533"
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
